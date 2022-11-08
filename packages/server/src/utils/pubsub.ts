ed9c0004847274aa0cfe3200f30c2dcb195e1a98
Nicolai Moraru <dot.nick.dot.messing@gmail.com>
2022-11-09 00:03:04 +0200

    Add db 0 to redis

--- packages/server/src/utils/pubsub.ts
+++ packages/server/src/utils/pubsub.ts
@@ -1,9 +1,11 @@
 import { RedisPubSub } from 'graphql-redis-subscriptions'
+
 export const pubsub = new RedisPubSub({
   connection: {
     host: process.env.REDIS_HOST,
     port: process.env.REDIS_PORT ? parseInt(process.env.REDIS_PORT) : 6379,
     username: process.env.REDIS_USERNAME,
     password: process.env.REDIS_PASSWORD,
+    db: 0
   }
 })
