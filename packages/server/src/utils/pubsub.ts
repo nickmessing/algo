842565f1b6212b360400c47e604c8d1b353c1bdf
Nicolai Moraru <dot.nick.dot.messing@gmail.com>
2022-11-08 23:40:22 +0200

    Add redis env vars

--- packages/server/src/utils/pubsub.ts
+++ packages/server/src/utils/pubsub.ts
@@ -1,2 +1,9 @@
 import { RedisPubSub } from 'graphql-redis-subscriptions'
-export const pubsub = new RedisPubSub()
+export const pubsub = new RedisPubSub({
+  connection: {
+    host: process.env.REDIS_HOST,
+    port: process.env.REDIS_PORT ? parseInt(process.env.REDIS_PORT) : 6379,
+    username: process.env.REDIS_USERNAME,
+    password: process.env.REDIS_PASSWORD,
+  }
+})
