d077f11564b64f68c4b07f38a5c1bd1240de6297
Nicolai Moraru <dot.nick.dot.messing@gmail.com>
2022-11-09 00:04:18 +0200

    Migrate to redis URL

--- packages/server/src/utils/pubsub.ts
+++ packages/server/src/utils/pubsub.ts
@@ -1,11 +1,5 @@
 import { RedisPubSub } from 'graphql-redis-subscriptions'
 
 export const pubsub = new RedisPubSub({
-  connection: {
-    host: process.env.REDIS_HOST,
-    port: process.env.REDIS_PORT ? parseInt(process.env.REDIS_PORT) : 6379,
-    username: process.env.REDIS_USERNAME,
-    password: process.env.REDIS_PASSWORD,
-    db: 0
-  }
+  connection: process.env.REDIS_URL
 })
